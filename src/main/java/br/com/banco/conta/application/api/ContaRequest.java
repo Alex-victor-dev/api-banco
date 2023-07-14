5f51eaf4150a1a7704eaecb138258ed29cf199b2
Alex-Victor <alexvictoribeiro@gmail.com>
2023-07-14 15:39:07 -0300

    modifica dominio

--- src/main/java/br/com/banco/conta/application/api/ContaRequest.java
+++ src/main/java/br/com/banco/conta/application/api/ContaRequest.java
@@ -3,6 +3,7 @@ package br.com.banco.conta.application.api;
 import javax.persistence.Column;
 import javax.validation.constraints.NotNull;
 
+import br.com.banco.conta.domain.TipoConta;
 import lombok.Data;
 
 @Data
@@ -10,5 +11,10 @@ public class ContaRequest {
 
 	@NotNull
 	@Column(name = "nome_responsavel")
-	private String nomeResponsavel;
+	private String nomeResponsavel;	
+	@Column(name = "saldo_conta")
+	private String saldoConta;
+	@NotNull
+	@Column(name = "tipo_conta", nullable = false)
+	private TipoConta tipoConta;
 }
---
+++ src/main/java/br/com/banco/conta/domain/TipoConta.java
@@ -0,0 +1,7 @@
+package br.com.banco.conta.domain;
+
+public enum TipoConta {
+	
+	POUPANCA, CORRENTE
+
+}
