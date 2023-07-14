4210402cd14ddb7434cfba9d512a24ccc589908d
Alex-Victor <alexvictoribeiro@gmail.com>
2023-07-14 15:18:59 -0300

    mapea conta

--- src/main/java/br/com/banco/conta/domain/Conta.java
+++ src/main/java/br/com/banco/conta/domain/Conta.java
@@ -1,13 +1,19 @@
 package br.com.banco.conta.domain;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 
 import br.com.banco.conta.application.api.ContaRequest;
+import br.com.banco.transferencia.domain.Transferencia;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,6 +31,9 @@ public class Conta {
 	@NotNull
 	@Column(name = "nome_responsavel")
 	private String nomeResponsavel;
+
+	@OneToMany(mappedBy = "conta", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Transferencia> transferencias;
 
 	public Conta(ContaRequest contaRequest) {
 		this.nomeResponsavel = contaRequest.getNomeResponsavel();
