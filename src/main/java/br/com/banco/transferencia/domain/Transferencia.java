8c95ed8ba011a9eaa2bf73464752ceed54375896
Alex-Victor <alexvictoribeiro@gmail.com>
2023-07-14 15:09:59 -0300

    mapea entidade de transferencia

--- src/main/java/br/com/banco/transferencia/domain/Transferencia.java
+++ src/main/java/br/com/banco/transferencia/domain/Transferencia.java
@@ -1,13 +1,18 @@
 package br.com.banco.transferencia.domain;
 
-import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+import br.com.banco.conta.domain.Conta;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,15 +26,21 @@ public class Transferencia {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", updatable = false, unique = true, nullable = false)
 	private Integer idTransferencia;
-	@Column(name = "data_transferencia")
-	private LocalDate dataTransferencia;
-	@Column(name = "valor")
-	private Double valorTransferencia;
-	@Column(name = "tipo")
+
+	@Column(name = "data_transferencia", nullable = false)
+	private LocalDateTime dataTransferencia;
+
+	@Column(name = "valor", nullable = false)
+	private BigDecimal valor;
+
+	@Column(name = "tipo", nullable = false)
 	private Tipo tipo;
+
 	@Column(name = "nome_operador_transacao")
-	private String nomeOperador;
-	@Column(name = "conta_id")
-	private Integer idConta;
+	private String nomeOperadorTransacao;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "conta_id", nullable = false)
+	private Conta conta;
 
 }
